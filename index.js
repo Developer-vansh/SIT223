82385cc1670f6fbfdb93c6fb122264f13b0f3f89
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-03-13 15:25:32 +0530

    ok

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ app.get('/style.css', function(req, res) {
 app.listen(8082,()=>{
     console.log("App listening", 8082)
 })
+app.listen(8084,()=>{
+    console.log("App listening", 8084)
+})
 
 module.exports = app;
